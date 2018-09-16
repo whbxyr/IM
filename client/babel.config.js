efdcd4d2554e73321b4b149c75722d147f7e453b
whbxyr <whbxyr@gmail.com>
2018-09-17 02:18:57 +0800

    调整 babel 配置文件以兼容 babel 7

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  // presets: [
-  //   ["@babel/preset-env", {
-  //     "modules": false
-  //   }],
-  //   ["@babel/preset-stage-2", {
-  //     decoratorsLegacy: true
-  //   }]
-  // ],
+  presets: [
+    ["@babel/preset-env", {
+      "modules": false
+    }],
+    // ["@babel/preset-stage-2", {
+    //   decoratorsLegacy: true
+    // }]
+    // ["@babel/plugin-proposal-decorators", {
+    //   "legacy": true
+    // }],
+    // "@babel/plugin-proposal-function-sent",
+    // "@babel/plugin-proposal-export-namespace-from",
+    // "@babel/plugin-proposal-numeric-separator",
+    // "@babel/plugin-proposal-throw-expressions"
+  ],
   plugins: ["transform-vue-jsx", "@babel/plugin-transform-runtime"]
 }
