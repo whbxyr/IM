917b4bd84a88672198907ba598a7a4a34faee346
whbxyr <whbxyr@gmail.com>
2019-04-23 23:55:26 +0800

    样式调整

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -107,13 +107,15 @@ export default {
 </script>
 
 <style lang="stylus">
-html
+html, body
   width 100%
   height 100%
+  margin 0
+  padding 0
 .login-ctn
   width 300px
   height 300px
-  margin 0 auto
+  margin 60px auto
 .room-ctn
   position fixed
   width 100%
